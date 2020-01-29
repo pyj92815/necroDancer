9927baf00e7bb3ca1225fe0711bb7ded898190c1
pyonnu <55772465+pyonnu@users.noreply.github.com>
2020-01-29 17:50:21 +0900

    20/01/29 PM(5:50) 집 가기전 푸쉬

--- enemy.h
+++ enemy.h
@@ -1,7 +1,6 @@
 #pragma once
 #include"animation.h"
 #include"gameNode.h"
-#include"Collision.h"
 #include<vector>
 enum class enemyState
 {
@@ -39,7 +38,6 @@ class Enemy:public gameNode
 protected:
 	EnemyInfo* _enemyInfo;				//에너미 정보(구조체)
 	playerInfo* _playerInfo;			//전달받아올 플레이어 정보(구조체) (좌표값만 받아옴)
-	Collision* _collision;
 public:
 	Enemy() {};
 	~Enemy() {};
